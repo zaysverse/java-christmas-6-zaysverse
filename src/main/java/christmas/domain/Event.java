c0834d4c90392fcd8147724b9d63d57cc4d4f9b4
zaysverse <zeze@jbnu.ac.kr>
2023-11-11 00:12:19 +0900

    feat : Event (enum) 클래스 생성
    12월 이벤트들을 enum으로 한 파일에 다 모아준다.
    
    - Order 클래스가 적용된 이벤트 리스트와 각 이벤트를 통해 할인받은 가격들을 담는 events(EnumMap)를 필드에 갖게 한다.

---
+++ src/main/java/christmas/domain/Event.java
@@ -0,0 +1,19 @@
+package christmas.domain;
+
+public enum Event {
+    CHRISTMAS_D_DAY ("크리스마스 디데이 할인"),
+    WEEKDAY("평일 할인"),
+    WEEKEND("주말 할인"),
+    SPECIAL("특별 할인"),
+    GIFT("증정 이벤트");
+
+    final private String message;
+
+    Event(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
--- src/main/java/christmas/domain/Order.java
+++ src/main/java/christmas/domain/Order.java
@@ -1,15 +1,19 @@
 package christmas.domain;
 
 import java.util.ArrayList;
+import java.util.EnumMap;
 import java.util.List;
+import java.util.Map;
 
 public class Order {
 
+    private int orderDate;
     private List<OrderMenu> orderMenus = new ArrayList<>();
     private Long totalPrice;
     private Long discountPrice;
-    private int orderDate;
     private EventBadge badge; // NONE, STAR, TREE, SANTA
+    private Map<Event, Long> events = new EnumMap<Event, Long>(Event.class); // CHRISTMAS_D_DAY, WEEKDAY, WEEKEND, SPECIAL, GIFT
+
 
     protected Order() {
         this.totalPrice = 0L;
@@ -19,6 +23,10 @@ public class Order {
     public void addOrderMenu(OrderMenu orderMenu) {
         orderMenus.add(orderMenu);
         totalPrice += orderMenu.getOrderPrice();
+    }
+
+    public void addEvent(Event event, Long discountPrice) {
+        events.put(event, discountPrice);
     }
 
     public void setOrderDate(int date) {
