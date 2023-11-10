b3400fe1d330f87156800e3ead6fd61444747ed0
zaysverse <zeze@jbnu.ac.kr>
2023-11-10 23:51:09 +0900

    feat( view ) : InputView, OutputView 클래스의 입출력 기능 추가
    입력, 출력 기능을 추가
    
    - 가격을 출력할때는 printPrice() 로 포맷터를 이용하는 함수 사용

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -1,16 +1,30 @@
 package christmas.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import christmas.repository.MenuRepository;
 
 import static camp.nextstep.edu.missionutils.Console.*;
 
 public class InputView {
+
+    private static InputView inputView;
+
+    public static InputView getInstance() {
+        if (inputView == null) {
+            inputView = new InputView();
+        }
+        return inputView;
+    }
+
     public int readDate() {
         System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
         String input = readLine();
-        // ...
-
-        return 1;
+        return Integer.parseInt(input);
     }
-    // ...
+
+    public String readOrder() {
+        System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
+        String input = readLine();
+        return input;
+    }
 }
--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -1,9 +1,56 @@
 package christmas.view;
 
+import java.text.DecimalFormat;
+
 public class OutputView {
-    public void printMenu() {
-        System.out.println("<주문 메뉴>");
-        // ...
+
+    private static OutputView outputView;
+
+    DecimalFormat formatter = new DecimalFormat("###,###원");
+
+    public static OutputView getInstance() {
+        if (outputView == null) {
+            outputView = new OutputView();
+        }
+        return outputView;
     }
-    // ...
+
+    public void printMenu(String output, Long totalPrice) {
+        System.out.println("<주문 메뉴>");
+        System.out.println(output);
+        System.out.println("<할인 전 총주문 금액>");
+        printPrice(totalPrice);
+    }
+
+    public void printGift(String output) {
+        System.out.println("<증정 메뉴>");
+        System.out.println(output);
+    }
+
+    public void printEvent(String output) {
+        System.out.println("<혜택 내역>");
+        System.out.println(output);
+    }
+
+    public void printDiscount(Long price) {
+        System.out.println("<총혜택 금액>");
+        printPrice(price);
+    }
+
+    public void printOrderPrice(Long price) {
+        System.out.println("<할인 후 예상 결제 금액>");
+        printPrice(price);
+    }
+
+    public void printBadge(String output) {
+        System.out.println("<12월 이벤트 배지>");
+        System.out.println(output);
+    }
+
+    public void printPrice(Long price) {
+        System.out.println(formatter.format(price));
+    }
+
+
+
 }
