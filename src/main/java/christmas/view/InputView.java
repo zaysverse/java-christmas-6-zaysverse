bc978318c98d89b01436da7cb92fe9744f4b42e3
zaysverse <zeze@jbnu.ac.kr>
2023-11-10 17:53:32 +0900

    feat(view) : 요구사항에 따라 InputView, OutputView 클래스 추가
    기재된 요구사항에 있는 view 코드를 복붙
    
    - 그에 따라 리드미 파일 약간 수정

---
+++ src/main/java/christmas/view/InputView.java
@@ -0,0 +1,16 @@
+package christmas.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import static camp.nextstep.edu.missionutils.Console.*;
+
+public class InputView {
+    public int readDate() {
+        System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
+        String input = readLine();
+        // ...
+
+        return 1;
+    }
+    // ...
+}
